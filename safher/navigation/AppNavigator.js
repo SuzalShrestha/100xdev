4bb86035007bb12e127e8f40b1614fca37bcfee4
sujalshrestha <sujalshresthawork@gmail.com>
2024-09-29 00:15:16 +0545

    home

--- safher/navigation/AppNavigator.js
+++ safher/navigation/AppNavigator.js
@@ -1,29 +1,26 @@
 import React from 'react';
-import { createStackNavigator } from '@react-navigation/stack';
-import { NavigationContainer } from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import {NavigationContainer} from '@react-navigation/native';
 import TabNavigator from './TabNavigator';
 import Login from '../screens/auth/login';
-
 const Stack = createStackNavigator();
-
 const AppNavigator = () => {
-    return (
-        <NavigationContainer>
-            <Stack.Navigator initialRouteName="Login">
-                <Stack.Screen
-                    name="Login"
-                    component={Login}
-                    options={{ headerShown: false }}
-                />
-                <Stack.Screen
-                    name="AfterAuth"
-                    component={TabNavigator}
-                    options={{ headerShown: false }}
-                />
-            </Stack.Navigator>
-
-        </NavigationContainer>
-    );
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen
+          name="Login"
+          component={Login}
+          options={{headerShown: false}}
+        />
+        <Stack.Screen
+          name="AfterAuth"
+          component={TabNavigator}
+          options={{headerShown: false}}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 };
 
 export default AppNavigator;
--- safher/navigation/TabNavigator.js
+++ safher/navigation/TabNavigator.js
@@ -1,31 +1,53 @@
 import React from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import HomeScreen from '../screens/afterAuth/HomeScreen';
-import SecondScreen from '../screens/afterAuth/SecondScreen';
+// import SecondScreen from '../screens/afterAuth/SecondScreen';
 import ThirdScreen from '../screens/afterAuth/ThirdScreen';
-import {Text} from 'react-native';
 import Profile from '../screens/afterAuth/profile';
 import FriendsScreen from '../screens/afterAuth/FriendsScreen';
 import Home from '../screens/auth/home/home';
-
+import HomeIcon from './home.svg';
+import MapIcon from './map.svg';
+import SOSICON from './sos.svg';
+import ProfileIcon from './profile.svg';
+import FriendsIcon from './friends.svg';
 const Tab = createBottomTabNavigator();
 
 const TabNavigator = () => {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={({route}) => ({
+        tabBarIcon: ({color, size}) => {
+          if (route.name === 'Home') {
+            return <HomeIcon size={size} color={color} />;
+          }
+          if (route.name === 'Maps') {
+            return <MapIcon size={size} color={color} />;
+          }
+          if (route.name === 'SOS') {
+            return <SOSICON size={size} color={color} />;
+          }
+          if (route.name === 'Profile') {
+            return <ProfileIcon size={size} color={color} />;
+          }
+          if (route.name === 'Friends') {
+            return <FriendsIcon size={size} color={color} />;
+          }
+          return <HomeIcon size={size} color={color} />;
+        },
+        tabBarActiveTintColor: 'tomato',
+        tabBarInactiveTintColor: 'gray',
+        headerShown: false,
+      })}>
       <Tab.Screen name="Home" component={Home} options={{headerShown: false}} />
+      <Tab.Screen name="Maps" component={HomeScreen} />
       <Tab.Screen
-        name="SecondScreen"
-        component={HomeScreen}
-        options={{headerShown: false}}
-      />
-      <Tab.Screen
-        name="Settings"
+        name="SOS"
         component={ThirdScreen}
         options={{headerShown: false}}
       />
       <Tab.Screen
-        name="UserProfile"
+        name="Profile"
         component={Profile}
         options={{headerShown: false}}
       />
